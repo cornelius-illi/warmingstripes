90f6d1dcb1f6fd0b94a8ac63bc5a6348c9bf35fa
Cornelius Illi <mail@corneliusilli.de>
2020-01-14 21:58:56 +0100

    update to include data from 2019

--- warmingstripes.py
+++ warmingstripes.py
@@ -4,29 +4,18 @@ import matplotlib.pyplot as plt
 
 ## get data from file
 ## this one is from dwd, and edited a little to be read with numpy
-## ftp://ftp-cdc.dwd.de/pub/CDC/regional_averages_DE/annual/air_temperature_mean/regional_averages_tm_year.txt
-data = np.loadtxt("regional_averages_tm_year.txt", skiprows=2)
-temps_bremen = data[:,8]
-temps_berlin_brandenburg= data[:,1]
-temps_germany = data[:,17]
+## https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/annual/air_temperature_mean/regional_averages_tm_year.txt
+## skip 2 header rows, select last column "Germany" only
+data = np.loadtxt("regional_averages_tm_year_1985-2019.txt", delimiter=';', skiprows=2, usecols=(18))
+stacked_temps = np.stack((data, data))
 
-## stack data to be able to plot them with imshow
-temps = temps_germany
-# temps = temps_bremen
-# temps = temps_berlin_brandenburg
-
-
-
-
-stacked_temps = np.stack((temps, temps))
-
-vmin = 5.9
-vmax = 11
+vmin = 6.5
+vmax = 11.2
 ## plotting
 ###############
-plt.figure(figsize=(4,18))
-# img = plt.imshow(stacked_temps, cmap='RdBu_r', aspect=40, vmin=vmin, vmax=vmax)
-img = plt.imshow(stacked_temps, cmap='RdBu_r', aspect=40, )
+plt.figure(figsize=(10,10))
+img = plt.imshow(stacked_temps, cmap='RdBu_r', aspect=12, vmin=vmin, vmax=vmax)
+#img = plt.imshow(stacked_temps, cmap='RdBu_r', aspect=40)
 
 plt.gca().set_axis_off()
 plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0,
@@ -34,6 +23,6 @@ plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0,
 plt.margins(0,0)
 plt.gca().xaxis.set_major_locator(plt.NullLocator())
 plt.gca().yaxis.set_major_locator(plt.NullLocator())
-plt.savefig("stripes.png", bbox_inches = 'tight',
+plt.savefig("stripes_germany_1985-2019.png", bbox_inches = 'tight',
     pad_inches = 0, dpi=400)
 
